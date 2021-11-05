3d4ed2e7f20b3d1ccb081986f975556d7cc3de32
Scott Bickish <sbickish3@gmail.com>
2021-11-05 12:04:25 -0600

    update

--- server/models/Post.js
+++ server/models/Post.js
@@ -5,6 +5,7 @@ export const PostSchema = new Schema({
   title: { type: String, required: true },
   creatorId: { type: Schema.Types.ObjectId, ref: 'Profile', required: true },
   imgUrl: { type: String },
+  gmapUrl: { type: String },
   discussion: { type: String, required: true },
   like: { type: Number },
   dislike: { type: Number }
--- server/services/PostsService.js
+++ server/services/PostsService.js
@@ -1,20 +1,31 @@
 import { dbContext } from '../db/DbContext'
-import { BadRequest } from '../utils/Errors'
+import { BadRequest, Forbidden } from '../utils/Errors'
 
 class PostsService {
   async getAll(query = {}) {
-    return await dbContext.Posts.find(query)
+    return await dbContext.Posts.find(query).populate('creator', 'name')
   }
 
   async getById(id) {
-    const post = await dbContext.Posts.findById(id)
+    const post = await dbContext.Posts.findById(id).populate('creator', 'name')
     if (!post) {
       throw new BadRequest('Invalid Id')
     }
+    return post
   }
 
   async create(body) {
+    // REVIEW do you want a populate here?
     return await dbContext.Posts.create(body)
+  }
+
+  async edit(body) {
+    const post = await this.getById(body.id)
+    if (post.creatorId.toString() !== body.creatorId) {
+      throw new Forbidden('you do not have the credentials')
+    }
+    const updatedPost = dbContext.Posts.findOneAndUpdate({ _id: body.id, creatorId: body.creatorId }, body, { new: true })
+    return updatedPost
   }
 }
 export const postsService = new PostsService()
