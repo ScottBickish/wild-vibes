89c898a799ee1047af8cc59a5e55df5ad5d86c79
Scott Bickish <sbickish3@gmail.com>
2021-11-05 12:03:49 -0600

    updates

--- server/controllers/PostsController.js
+++ server/controllers/PostsController.js
@@ -4,12 +4,14 @@ import BaseController from '../utils/BaseController'
 
 export class PostsController extends BaseController {
   constructor() {
-    super('api/posts')
+    super('api/wildvibes/posts')
     this.router
       .get('', this.getAll)
       .get('/:id', this.getById)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.create)
+      .put('/:id', this.edit)
+      .delete()
   }
 
   async getAll(req, res, next) {
@@ -35,6 +37,17 @@ export class PostsController extends BaseController {
     try {
       req.body.creatorId = req.userInfo.id
       const post = await postsService.create(req.body)
+      return res.send(post)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async edit(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      req.body.id = req.params.id
+      const post = await postsService.edit(req.body)
       return res.send(post)
     } catch (error) {
       next(error)
