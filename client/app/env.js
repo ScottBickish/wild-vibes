39adafd6c70493889e78039bd493c0e35eed23e0
Scott Bickish <sbickish3@gmail.com>
2021-11-05 10:42:31 -0600

    update get getid and post

--- client/app/env.js
+++ client/app/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'scott-codeworks.us.auth0.com'
+export const audience = 'https://ScottDev.com'
+export const clientId = 'RCyA1uqr2CFegPI99M78Pp4D6sx08pJA'
---
+++ server/controllers/PostsController.js
@@ -0,0 +1,43 @@
+import { Auth0Provider } from '@bcwdev/auth0provider'
+import { postsService } from '../services/PostsService'
+import BaseController from '../utils/BaseController'
+
+export class PostsController extends BaseController {
+  constructor() {
+    super('api/posts')
+    this.router
+      .get('', this.getAll)
+      .get('/:id', this.getById)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.create)
+  }
+
+  async getAll(req, res, next) {
+    try {
+      const query = req.query
+      const posts = await postsService.getAll(query)
+      return res.send(posts)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getById(req, res, next) {
+    try {
+      const post = await postsService.getById(req.params.id)
+      return res.send(post)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(req, res, next) {
+    try {
+      req.body.creatorId = req.userInfo.id
+      const post = await postsService.create(req.body)
+      return res.send(post)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
