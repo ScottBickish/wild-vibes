8839ec4f316c1c35f061aa9f0b81f270006e29e7
Ryan Fitzgerald <ryfitz@ymail.com>
2021-11-05 22:07:29 -0600

    post

--- client/app/Services/CommentsService.js
+++ client/app/Services/CommentsService.js
@@ -1,15 +1,19 @@
+import { ProxyState } from '../AppState.js'
+import { Comment } from '../Models/Comment.js'
 import { logger } from '../Utils/Logger.js'
 import { api } from './AxiosService.js'
 
 class CommentsService {
   async getAllComments() {
     const res = await api.get('api/wildvibes/comments')
+    ProxyState.comments = res.data.map(c => new Comment(c))
     logger.log(res.data)
   }
 
   async createComment(data) {
     const res = await api.post('api/wildvibes/comments', data)
-    logger.log(res.data)
+    ProxyState.comments = [new Comment(res.data), ...ProxyState.comments]
+    logger.log('create comment', res.data)
   }
 
   //   async editcomment(neData) {
