9bec45d5ba339db092ff914493c0192e32b23a7b
John Melo <joaolucasrmelo@gmail.com>
2021-11-05 19:41:49 -0600

    john

--- client/app/Models/Comment.js
+++ client/app/Models/Comment.js
@@ -10,19 +10,13 @@ export class Comment {
     return `
     <div class="card border-0 mt-2 commentsbutton text-start p-3 mb-4 mb-1">
     <div class="d-flex justify-content-between">
-      <h6 class=" mb-1">You Commented:</h6>
+      <h6 class=" mb-1">${this.creatorName} Commented:</h6>
       <div>
-        <i class=" mdi m-1 mdi-pencil "></i>
-        <i class="m-1 mdi  mdi-trash-can "></i>
+        <i class=" selectable mdi m-1 mdi-pencil "></i>
+        <i class="m-1 mdi selectable mdi-trash-can "></i>
       </div>
     </div>
-    <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-      been
-      the
-      industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
-      type
-      and
-      scrambled it to make a type specimen book. </p>
+    <p> ${this.comment} </p>
   </div>
 </div> 
     `
